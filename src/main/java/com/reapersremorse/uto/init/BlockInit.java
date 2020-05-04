b1aad8660abea061447a2926b68250bac8892480
reapersremorse <reapersremorse@gmail.com>
2020-05-04 00:56:16 -0400

    many blocks and items
    Blocks and Items added, Block models for item and block have not been made yet. ill work on that later today. i need sleep.

---
+++ src/main/java/com/reapersremorse/uto/init/BlockInit.java
@@ -0,0 +1,115 @@
+package com.reapersremorse.uto.init;
+
+import com.reapersremorse.uto.UTOMod;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ObjectHolder;
+
+@Mod.EventBusSubscriber(modid = UTOMod.MOD_ID,bus = Mod.EventBusSubscriber.Bus.MOD)
+@ObjectHolder(UTOMod.MOD_ID)
+public class BlockInit
+{
+    //TODO: block,bricks,chiseled bricks, cobble, cracked, end, smooth, stone
+    public static final Block uto_block = null;
+    public static final Block uto_bricks = null;
+    public static final Block uto_chiseled_bricks = null;
+    public static final Block uto_cobblestone = null;
+    public static final Block uto_cracked_bricks = null;
+    public static final Block uto_end_stone = null;
+    public static final Block uto_ore = null;
+    public static final Block uto_smooth = null;
+    public static final Block uto_stone = null;
+
+    public static final Block uto_covalence_block_white = null;
+    public static final Block uto_covalence_block_orange = null;
+    public static final Block uto_covalence_block_magenta = null;
+    public static final Block uto_covalence_block_light_blue = null;
+    public static final Block uto_covalence_block_yellow = null;
+    public static final Block uto_covalence_block_lime = null;
+    public static final Block uto_covalence_block_pink = null;
+    public static final Block uto_covalence_block_gray = null;
+    public static final Block uto_covalence_block_light_gray = null;
+    public static final Block uto_covalence_block_cyan = null;
+    public static final Block uto_covalence_block_purple = null;
+    public static final Block uto_covalence_block_blue = null;
+    public static final Block uto_covalence_block_brown = null;
+    public static final Block uto_covalence_block_green = null;
+    public static final Block uto_covalence_block_red = null;
+    public static final Block uto_covalence_block_black = null;
+
+    @SubscribeEvent
+    public static void registerItems(final RegistryEvent.Register <Block>event)
+    {
+        //basic blocks
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_block"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_bricks"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_chiseled_bricks"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_cobblestone"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_cracked_bricks"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_end_stone"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_ore"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_smooth"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_stone"));
+
+        //covalence dusts blocks
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_white"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_orange"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_magenta"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_light_blue"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_yellow"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_lime"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_pink"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_gray"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_light_gray"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_cyan"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_purple"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_blue"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_brown"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_green"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_red"));
+        event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1,1.0f).harvestLevel(0).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)).setRegistryName("uto_covalence_block_black"));
+
+    }
+    @SubscribeEvent
+    public static void registerItemBlocks(final RegistryEvent.Register <Item>event)
+    {
+        //basic blocks
+        event.getRegistry().register(new BlockItem(uto_block,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_block"));
+        event.getRegistry().register(new BlockItem(uto_bricks,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_bricks"));
+        event.getRegistry().register(new BlockItem(uto_chiseled_bricks,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_chiseled_bricks"));
+        event.getRegistry().register(new BlockItem(uto_cobblestone,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_cobblestone"));
+        event.getRegistry().register(new BlockItem(uto_cracked_bricks,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_cracked_bricks"));
+        event.getRegistry().register(new BlockItem(uto_end_stone,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_end_stone"));
+        event.getRegistry().register(new BlockItem(uto_ore,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_ore"));
+        event.getRegistry().register(new BlockItem(uto_smooth,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_smooth"));
+        event.getRegistry().register(new BlockItem(uto_stone,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_stone"));
+
+        //covalence dusts blocks
+        event.getRegistry().register(new BlockItem(uto_covalence_block_white,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_white"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_orange,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_orange"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_magenta,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_magenta"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_light_blue,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_light_blue"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_yellow,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_yellow"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_lime,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_lime"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_pink,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_pink"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_gray,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_gray"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_light_gray,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_light_gray"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_cyan,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_cyan"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_purple,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_purple"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_blue,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_blue"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_brown,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_brown"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_green,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_green"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_red,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_red"));
+        event.getRegistry().register(new BlockItem(uto_covalence_block_black,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_block_black"));
+
+    }
+
+}
--- src/main/java/com/reapersremorse/uto/init/ItemInit.java
+++ src/main/java/com/reapersremorse/uto/init/ItemInit.java
@@ -12,12 +12,73 @@ import net.minecraftforge.registries.ObjectHolder;
 @ObjectHolder(UTOMod.MOD_ID)
 public class ItemInit
 {
-    //@ObjectHolder(UTOMod.MOD_ID+"example_item")
-    public static final Item example_item = null;
+    //TODO: add transmogrified elements (kinda like: earth, air, fire, water, spirit , electric)
+    //normal items
+    public static final Item uto_generic_apple = null;
+    public static final Item uto_generic_bowl = null;
+    public static final Item uto_generic_eye = null;
+    public static final Item uto_generic_fuel = null;
+    public static final Item uto_generic_gem = null;
+    public static final Item uto_generic_ingot = null;
+    public static final Item uto_generic_nugget = null;
+    public static final Item uto_generic_pearl = null;
+    public static final Item uto_generic_rod = null;
+    public static final Item uto_generic_slime_ball = null;
+    public static final Item uto_generic_stick = null;
+    public static final Item uto_generic_string = null;
+    public static final Item uto_generic_dust = null;
+    //covalence dusts
+    public static final Item uto_covalence_dust_white = null;
+    public static final Item uto_covalence_dust_orange = null;
+    public static final Item uto_covalence_dust_magenta = null;
+    public static final Item uto_covalence_dust_light_blue = null;
+    public static final Item uto_covalence_dust_yellow = null;
+    public static final Item uto_covalence_dust_lime = null;
+    public static final Item uto_covalence_dust_pink = null;
+    public static final Item uto_covalence_dust_gray = null;
+    public static final Item uto_covalence_dust_light_gray = null;
+    public static final Item uto_covalence_dust_cyan = null;
+    public static final Item uto_covalence_dust_purple = null;
+    public static final Item uto_covalence_dust_blue = null;
+    public static final Item uto_covalence_dust_brown = null;
+    public static final Item uto_covalence_dust_green = null;
+    public static final Item uto_covalence_dust_red = null;
+    public static final Item uto_covalence_dust_black = null;
 
     @SubscribeEvent
     public static void registerItems(final RegistryEvent.Register <Item> event)
     {
-        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("example_item"));
+        //regular items
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_generic_apple"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_generic_bowl"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_generic_eye"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_generic_fuel"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_generic_gem"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_generic_ingot"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_generic_nugget"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_generic_pearl"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_generic_rod"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_generic_slime_ball"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_generic_stick"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_generic_string"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_generic_dust"));
+
+        //covalence dusts
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_white"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_orange"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_magenta"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_light_blue"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_yellow"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_lime"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_pink"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_gray"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_light_gray"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_cyan"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_purple"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_blue"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_brown"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_green"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_red"));
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("uto_covalence_dust_black"));
     }
 }
